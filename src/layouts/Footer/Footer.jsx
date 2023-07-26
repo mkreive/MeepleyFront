6c297fa18be832525c6e2a8fb325d9cdd563ae6b
mkreive <mkreive@gmail.com>
2023-07-26 15:47:18 +0300

    home page and not found completed

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,12 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import classNames from 'classnames/bind';
 import styles from './footer.module.scss';
+import Logo from '../../components/Logo/Logo';
+import Paragraph from '../../components/Paragraph/Paragraph';
 
 const cn = classNames.bind(styles);
 
-export default function Footer() {
+export default function Header() {
+    const isMobile = useMediaQuery({ query: '(max-width: 700px)' });
+
     return (
-        <footer className={cn('container')}>
-            <p>Footer</p>
-        </footer>
+        <header className={cn('container')}>
+            <div className={cn('wrapper')}>
+                <Paragraph style='regular--white'>Copyright © 2023 MEEPLEY</Paragraph>
+                <nav className={cn('nav')}>
+                    <Link className={cn('link')} to='/home'>
+                        Home
+                    </Link>
+                    <Link className={cn('link')} to='/games'>
+                        Games
+                    </Link>
+                    <Link className={cn('link')} to='/forums'>
+                        Forums
+                    </Link>
+                    {/* <Link className={cn('link')} to='/account'>
+                            Account
+                        </Link>
+                        <Link className={cn('link')} to='/services'>
+                            Services
+                        </Link> */}
+                    <Link className={cn('link')} to='/login'>
+                        Login
+                    </Link>
+                </nav>
+            </div>
+        </header>
     );
 }
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './not-found-page.module.scss';
+import NotFoundImage from './NotFoundImage';
+import Heading from '../../components/Heading/Heading';
+import { Link } from 'react-router-dom';
 
 const cn = classNames.bind(styles);
 
 export default function NotFoundPage() {
     return (
         <div className={cn('container')}>
-            <p>Not Found Page</p>
+            <NotFoundImage />
+            <Heading tag='h1' style='medium'>
+                Sorry, looks like the page you are trying to reach no longer exists.
+            </Heading>
+            <Heading tag='h2' style='small'>
+                Let's get you{' '}
+                <Link to={'/home'} className={cn('link')}>
+                    home
+                </Link>
+                .
+            </Heading>
         </div>
     );
 }
