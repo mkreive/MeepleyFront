64c3a37aee0c0367c87428661228c380518ed7cf
mkreive <mkreive@gmail.com>
2023-08-01 15:48:39 +0300

    auth layout created

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, useNavigate } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import styles from './app.module.scss';
 import classNames from 'classnames/bind';
 import { oktaConfig } from '../lib/oktaConfig';
-import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
-import { Security, LoginCallback } from '@okta/okta-react';
+import { LoginCallback } from '@okta/okta-react';
 import RootLayout from '../layouts/RootLayout/RootLayout';
+import AuthLayout from '../Auth/AuthLayout';
 import HomePage from '../pages/HomePage/HomePage';
 import GamesPage from '../pages/GamesPage/GamesPage';
 import GameCheckoutPage from '../pages/GameCheckoutPage/GameCheckoutPage';
@@ -13,31 +13,22 @@ import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 import LoginWidget from '../Auth/LoginWidget';
 
 const cn = classNames.bind(styles);
-const oktaAuth = new OktaAuth(oktaConfig);
-const customAuthHandler = () => {
-    history.push('/login');
-};
-const restoreOriginalUri = async () => {
-    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
-};
+
 const router = createBrowserRouter(
     createRoutesFromElements(
-        <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
-            <Route path='/' element={<RootLayout />}>
-                <Route index element={<HomePage />} />
-                <Route path='/home' element={<HomePage />} />
-                <Route path='/games' element={<GamesPage />} />
-                <Route path='/checkout/:gameId' element={<GameCheckoutPage />} />
-                <Route path='/login' element={<LoginWidget config={oktaConfig} />} />
-                <Route path='/login/callback' element={LoginCallback} />
-                <Route path='*' element={<NotFoundPage />} />
-            </Route>
-        </Security>
+        <Route path='/' element={<AuthLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path='/home' element={<HomePage />} />
+            <Route path='/games' element={<GamesPage />} />
+            <Route path='/checkout/:gameId' element={<GameCheckoutPage />} />
+            <Route path='/login' element={<LoginWidget config={oktaConfig} />} />
+            <Route path='/login/callback' element={LoginCallback} />
+            <Route path='*' element={<NotFoundPage />} />
+        </Route>
     )
 );
 
 export default function App() {
-    const history = useNavigate();
     return (
         <div className={cn('app')}>
             <RouterProvider router={router} />
---
+++ src/Auth/AuthLayout.jsx
@@ -0,0 +1,32 @@
+import { Outlet, useNavigate } from 'react-router-dom';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { Security } from '@okta/okta-react';
+import { oktaConfig } from '../lib/oktaConfig';
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './auth-layout.module.scss';
+import Footer from '../layouts/Footer/Footer';
+import Header from '../layouts/Header/Header';
+
+const cn = classNames.bind(styles);
+
+export default function AuthLayout() {
+    const history = useNavigate();
+    const oktaAuth = new OktaAuth(oktaConfig);
+    const customAuthHandler = () => {
+        history.push('/login');
+    };
+    const restoreOriginalUri = async () => {
+        history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+    };
+
+    return (
+        <div className={cn('container')}>
+            <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
+                <Header />
+                <Outlet />
+                <Footer />
+            </Security>
+        </div>
+    );
+}
