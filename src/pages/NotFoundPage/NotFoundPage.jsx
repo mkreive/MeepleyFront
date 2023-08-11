74bf71794593674bdd5fde79308a05fe45a65aab
mkreive <mkreive@gmail.com>
2023-08-11 11:57:08 +0300

    translations finished

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import classNames from 'classnames/bind';
 import styles from './not-found-page.module.scss';
 import NotFoundImage from './NotFoundImage';
@@ -8,16 +9,17 @@ import { Link } from 'react-router-dom';
 const cn = classNames.bind(styles);
 
 export default function NotFoundPage() {
+    const { t } = useTranslation();
     return (
         <div className={cn('container')}>
             <NotFoundImage />
             <Heading tag='h1' style='medium'>
-                Sorry, looks like the page you are trying to reach no longer exists.
+                {t('notfoundpage_heading')}
             </Heading>
             <Heading tag='h2' style='small'>
-                Let's get you{' '}
+                {t('notfoundpage_letsgetyou')}
                 <Link to={'/home'} className={cn('link')}>
-                    home
+                    {t('notfoundpage_link')}
                 </Link>
                 .
             </Heading>
